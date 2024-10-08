b0787211b58bbacaffdf43a481b470330e983531
chetnxp <chetanpatil0430@gmail.com>
2024-10-08 22:11:27 +0530

    project

---
+++ src/employee/management/system/Conn.java
@@ -0,0 +1,26 @@
+package employee.management.system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn{
+
+
+    Connection connection;
+
+    Statement statement;
+
+    public Conn (){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/employeemanagement","root","123456789");
+            statement = connection.createStatement();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
